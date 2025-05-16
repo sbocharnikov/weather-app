ccfdf5cab98092116cf732af34b078f953a764da
sbocharnikov <44550584+sbocharnikov@users.noreply.github.com>
2025-05-16 16:37:19 +0300

    handle wrong subscription request

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, HttpException, HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: () =>
+        new HttpException('Invalid input', HttpStatus.BAD_REQUEST),
+      stopAtFirstError: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
