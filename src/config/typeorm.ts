38d3c103ebf3f5f774fffb44de9ee3e548c5351e
sbocharnikov <44550584+sbocharnikov@users.noreply.github.com>
2025-05-19 18:50:44 +0300

    update db config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,7 +6,7 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: parseInt(process.env.DATABASE_PORT || '5432', 10),
   username: 'postgres',
   password: 'postgres',
